77db9540330e318a9c73105e0098fc9a72877874
Dan <danishasad91@gmail.com>
2024-04-01 20:27:34 +0530

    Added Components

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,21 @@
-import Image from 'next/image';
-import { Nav } from './ui/Nav';
+import Nav from './ui/Nav';
+import Home from './ui/Home';
+import Work from './ui/Work';
+import ToolChain from './ui/ToolChain';
+import Contacts from './ui/Contacts';
+import Footer from './ui/Footer';
 
-export default function Home() {
+export default function Page() {
 	return (
-		<main>
+		<>
 			<Nav />
-		</main>
+			<main className='px-6 space-y-16'>
+				<Home />
+				<Work />
+				<ToolChain />
+				<Contacts />
+			</main>
+			<Footer />
+		</>
 	);
 }
