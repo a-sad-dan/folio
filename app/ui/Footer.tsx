287e2b18f8499bd24a2731b2c181324237c2d70e
Dan <danishasad91@gmail.com>
2024-04-01 20:26:31 +0530

    added component

---
+++ app/ui/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+	return (
+		<footer className="pb-16 p-10">
+			<p className='text-sm text-center'>{'Designed and Developed with <3'}</p>
+		</footer>
+	);
+};
+
+export default Footer;
