0424513ab7cd610e3714a3acf7843c7804cf9a3b
Dan <danishasad91@gmail.com>
2024-04-05 15:02:45 +0530

    Add Section Component

--- app/ui/Work.tsx
+++ app/ui/Work.tsx
@@ -1,27 +1,42 @@
-import Image from 'next/image';
-import scrollDown from '@/app/assets/scroll-down-2.svg';
-	
+import WorkCard from './components/WorkCard';
+import minimalist from '@/app/assets/mockups/minimalist-tablet.png';
+import topRealty from '@/app/assets/mockups/top-realty.png';
+import weatherNaut from '@/app/assets/mockups/weathernaut.png';
+import calculator from '@/app/assets/mockups/calculator-green.png';
+
 const Work = () => {
 	return (
-		<section className='space-y-8'>
-			<div className='space-y-1'>
+		<section className='space-y-8 mx-auto w-max'>
+			<div>
 				<h2 className='text-xl font-bold pt-8 pb-3.5 text-center'>Work</h2>
-				<p className='text-base text-center'>
-					Collection of my favourite Works
-				</p>
+				<p className='text-base text-center'>Selected Works</p>
 			</div>
-			<div className='relative'>
-				<p>
-					My passion lies in crafting purpose-driven digital experiences that
-					not only meet essential objectives but also empower with a unique
-					competitive advantage.
-				</p>
-				<Image
-					src={scrollDown}
-					alt='scroll down'
-					height={75}
-					width={75}
-					className='absolute  -left-4 -bottom-3/4'
+			<div className='space-y-8 sm:space-y-0 sm:flex sm:gap-8 sm:flex-wrap sm:'>
+				<WorkCard
+					name='MinimaList'
+					date='16/01/2024'
+				liveLink='https://a-sad-dan.github.io/minima-List/'
+				photo={minimalist}
+				/>
+				<WorkCard
+					name='Top Realty'
+					date='21/10/2023'
+					liveLink='https://toprealtymumbai.com'
+					photo={topRealty}
+				/>
+				<WorkCard 
+				name='WeatherNaut'
+				date='19/11/23'
+				liveLink='https://a-sad-dan.github.io/weatherNaut'
+				photo={weatherNaut}
+				/>
+				<WorkCard
+					name='Calculator'
+					date='09/07/2023'
+					liveLink='https://a-sad-dan.github.io/Calculator/'
+					photo={calculator}
+				/>
+				<WorkCard
 				/>
 			</div>
 		</section>
