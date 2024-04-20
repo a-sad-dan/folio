e940af70e0884a0b2c6bb09979c7bdf3f1ab9bc4
Dan <danishasad91@gmail.com>
2024-04-20 14:39:32 +0530

    Added App opening on photo click, added source link prop

--- app/ui/components/WorkCard.tsx
+++ app/ui/components/WorkCard.tsx
@@ -5,15 +5,23 @@ import tape from '@/app/assets/tape.svg';
 
 import dirtyTexture from '@/app/assets/textures/dirty-texture.jpg';
 import polaroidTexture from '@/app/assets/textures/Texturelabs_Glass_133S.jpg';
+import clsx from 'clsx';
 
 interface WorkCardProps {
 	liveLink?: string;
+	sourceLink?: string;
 	name?: string;
 	date?: string;
 	photo?: StaticImageData;
 }
 
-const WorkCard = ({ liveLink, name, date, photo }: WorkCardProps) => {
+const WorkCard = ({
+	liveLink,
+	sourceLink,
+	name,
+	date,
+	photo,
+}: WorkCardProps) => {
 	const defaultLink =
 		'https://www.youtube.com/watch?v=l4ezx3omYeA&pp=ygUfaG93IGEgcG9scm9pZCBmaWxtIGlzIGRldmVsb3BlZA%3D%3D';
 
@@ -46,32 +54,69 @@ const WorkCard = ({ liveLink, name, date, photo }: WorkCardProps) => {
 			</div>
 			<div className='bg-[#fffdf5] px-4 py-7 w-max space-y-3 shadow-lg'>
 				<div className='relative'>
-					<Image
-						src={photo || mock}
-						alt='photo of App'
-						height={270}
-						width={210}
-						className='h-60 object-cover saturate-50 sepia-[0.2] brightness-[0.8] shadow-inner z-10'
-					/>
-					<Image
-						src={polaroidTexture}
-						alt='polaroid texture'
-						height={270}
-						width={210}
-						className='absolute top-0 h-60 object-cover opacity-15 shadow-inner z-10'
-					/>
-				</div>
-				<div className='flex justify-between'>
 					<a
 						href={liveLink || defaultLink}
 						target='_blank'
 					>
 						<Image
+							src={photo || mock}
+							alt='photo of App'
+							height={270}
+							width={210}
+							className='h-60 object-cover saturate-50 sepia-[0.2] brightness-[0.8] shadow-inner z-10'
+						/>
+						<Image
+							src={polaroidTexture}
+							alt='polaroid texture'
+							height={270}
+							width={210}
+							className='absolute top-0 h-60 object-cover opacity-15 shadow-inner z-10'
+						/>
+					</a>
+				</div>
+				<div className='flex justify-between'>
+					<a
+						href={sourceLink}
+						target='_blank'
+						className={clsx({'collapse' : !sourceLink})}
+					>
+						{/* <Image
 							src={goToIcon}
 							alt='Open in new tab icon'
 							height={26}
 							width={26}
-						/>
+						/> */}
+						<svg
+							xmlns='http://www.w3.org/2000/svg'
+							width='20'
+							height='20'
+							viewBox='0 0 24 24'
+							fill='none'
+							stroke='currentColor'
+							stroke-width='2'
+							stroke-linecap='round'
+							stroke-linejoin='round'
+							class='lucide lucide-braces mt-1 text-text'
+						>
+							<rect
+								x='14'
+								y='14'
+								width='4'
+								height='6'
+								rx='2'
+							/>
+							<rect
+								x='6'
+								y='4'
+								width='4'
+								height='6'
+								rx='2'
+							/>
+							<path d='M6 20h4' />
+							<path d='M14 10h4' />
+							<path d='M6 14h2v6' />
+							<path d='M14 4h2v6' />
+						</svg>
 					</a>
 					<div className='text-right font'>
 						<h4 className='text-lg font-bold'>{name || 'Under Dev'}</h4>
